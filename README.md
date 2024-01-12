ab6313bf3e74bbb492d02809a27bd63a8832765b
your username <your email>
2024-01-12 11:08:01 -0700

    Finished main assignment

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # cse210-student-template
-This is the starter code for students in CSE 210.
+Corban Critchfield is a real homie g. Dap me up brotha!
 
 This repository contains the starter code for many different projects. They are arranged as follows:
 
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,6 +5,45 @@ class Program
     static void Main(string[] args)
     {
         // This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your final grade? ");
+        string UserInput = Console.ReadLine();
+        int FinalGrade = int.Parse(UserInput);
+
+        int A = 90;
+        int B = 80;
+        int C = 70;
+        int D = 60;
+
+        if (FinalGrade >= A)
+        {
+            Console.Write("You got an A! ");
+        }
+        else if (A > FinalGrade && FinalGrade >= B )
+        {
+            Console.Write("You got a B! ");
+        }
+        else if (B > FinalGrade && FinalGrade >= C)
+        {
+            Console.Write("You got a C! ");
+        }
+        else if (C > FinalGrade && FinalGrade >= D)
+        {
+            Console.Write("You got a D. ");
+        }
+        else
+        {
+            Console.Write("You got an F. ");
+        }
+
+
+        if (FinalGrade >= C)
+        {
+            Console.Write("You also got a passing grade! ");
+        }
+        else
+        {
+            Console.Write("You did not pass. ");
+        }
+        
     }
 }
