3deca36d5166d19998f0b7475f2b634d34e17060
your username <your email>
2024-01-17 22:23:26 -0700

    Finished Prep 4 Assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
+    { 
+    List<int> numbers = new List<int>();
+    int numberOfInputs = 0;
+    int total = 0;
+    int userNumber = 0;
+    int average = 0;
+    Console.WriteLine("Enter a list of numbers. Type 0 when you are finished.");
+    do 
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a number: ");
+        string userInput = Console.ReadLine();
+        userNumber = int.Parse(userInput);
+        if (userNumber !=  0)
+        {
+            numbers.Add(userNumber);
+            total += userNumber;
+            numberOfInputs += 1;
+        }
+    } while (userNumber != 0);
+
+    Console.WriteLine($"The total is {total}.");
+
+    average = total/numberOfInputs;
+    Console.WriteLine($"The average is {average}.");
+
+    int largestNumber = numbers.Max();
+    Console.WriteLine($"The largest number in the list is {largestNumber}.");
+
+        // Lists - new keyword
+        // List <int> MyInts = new List<int>();
+        // var otherInts = new List<int>();
+
+        // // Add items
+        // MyInts.Add(10);
+        // MyInts.Add(3); //Don't add floats becasue we already specified it's an int list
+        // MyInts.Add(45);
+
+        // // Iterate over items
+        // foreach (var n in MyInts)
+        // {
+        //     System.Console.WriteLine($"n = {n}");
+        // }
+
+
+
+        // Console.WriteLine("Hello Prep4 World!");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // functions
+        // return int
+        int Add2 (int number, string name)
+        {
+            return number + 2;
+        }
+        // void
+        void PrintName(string name)
+        {
+            System.Console.WriteLine(name);
+        }
+        int answer = Add2(10,"bob");
+        PrintName($"Bob is {answer}");
+        // Variable Scope
+        var i = 12;
+        {
+            var y =10;
+            y = i + 3;
+            i = y + 4;
+        }
     }
 }
